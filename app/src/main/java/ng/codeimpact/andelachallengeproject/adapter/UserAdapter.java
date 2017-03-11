cdb176df1a7805056a2af8c6fa35d4b06d3bdaf6
brownjee <nsikakthompson73@gmail.com>
2017-03-11 10:50:53 +0100

    Loaded the image to the ImageView using Glide

--- app/src/main/java/ng/codeimpact/andelachallengeproject/adapter/UserAdapter.java
+++ app/src/main/java/ng/codeimpact/andelachallengeproject/adapter/UserAdapter.java
@@ -10,9 +10,12 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 import ng.codeimpact.andelachallengeproject.R;
+import ng.codeimpact.andelachallengeproject.model.User;
 
 /**
  * Created by Nsikak  Thompson on 3/11/2017.
@@ -20,57 +23,59 @@ import ng.codeimpact.andelachallengeproject.R;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
 
-    private List<Home_item> moviesList;
+    private List<User> moviesList;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageView imgItem;
-        TextView txtItem,  description;
-        RelativeLayout layer;
+        ImageView user_profile_avatar;
+        TextView username, user_state;
+
 
         public MyViewHolder(View view) {
             super(view);
-            imgItem = (ImageView) view.findViewById(R.id.thumbnail);
-            txtItem = (TextView) view.findViewById(R.id.title);
-            layer = (RelativeLayout) view.findViewById(R.id.layer);
+            user_profile_avatar = (ImageView) view.findViewById(R.id.profile_image);
+            username = (TextView) view.findViewById(R.id.user_name);
+            user_state = (TextView) view.findViewById(R.id.userstate);
 
         }
     }
 
 
-    public UserAdapter(List<Home_item> moviesList) {
+    public UserAdapter(List<User> moviesList) {
         this.moviesList = moviesList;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.homelist_item, parent, false);
+                .inflate(R.layout.userlist_item, parent, false);
 
         return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int position) {
-        Home_item item = moviesList.get(position);
+        User user = moviesList.get(position);
 
-        /// holder.layer.setBackgroundColor(item.getColor());
-        // Toast.makeText(holder.layer.getContext(), item.getColor(), Toast.LENGTH_SHORT).show();
+        holder.username.setText(user.getLogin());
+        holder.user_state.setText("Lagos");
 
 
-        Context context = holder.txtItem.getContext();
+        //Loading the image using Glide
+        Context context = holder.user_profile_avatar.getContext();
+        Glide.with(context).load(user.getAvatarUrl()).into(holder.user_profile_avatar);
+
+
+       /* Context context = holder.txtItem.getContext();
         ChronicaProRegularHelper.applyFont(context, holder.txtItem);
         holder.imgItem.setImageResource(item.getImage());
         holder.txtItem.setText(item.getTitle());
-
+*/
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Context context = holder.itemView.getContext();
 
-                if(position ==0 ){
-                    context.startActivity(new Intent(context, CategoryActivity.class));
-                }
 
             }
         });
