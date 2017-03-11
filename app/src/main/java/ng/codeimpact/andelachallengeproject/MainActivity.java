7ad2eea016e7f051c5d3c962a3e88483e182295f
brownjee <nsikakthompson73@gmail.com>
2017-03-11 13:41:28 +0100

    Feat: Checked for network connectivity

--- app/src/main/java/ng/codeimpact/andelachallengeproject/MainActivity.java
+++ app/src/main/java/ng/codeimpact/andelachallengeproject/MainActivity.java
@@ -1,15 +1,44 @@
 package ng.codeimpact.andelachallengeproject;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.GridLayoutAnimationController;
+import android.widget.Toast;
+
+import ng.codeimpact.andelachallengeproject.adapter.UserAdapter;
+import ng.codeimpact.andelachallengeproject.model.UserList;
+import ng.codeimpact.andelachallengeproject.service.RestApiBuilder;
+import ng.codeimpact.andelachallengeproject.service.RestApiService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
+    private CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +55,73 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_user_list);
+        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        if (!isNetworkAvailable()) {
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, "No Network connection", Snackbar.LENGTH_LONG)
+                    .setAction("RETRY", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            fetchUsersData();
+                        }
+                    });
+
+            snackbar.show();
+        } else {
+            fetchUsersData();
+        }
+
+
+    }
+
+
+    private void prepareData(UserList userList) {
+        adapter = new UserAdapter(userList.getItems());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+    private void fetchUsersData() {
+        String searchParams = "language:java location:lagos";
+        RestApiService apiService = new RestApiBuilder().getService();
+        Call<UserList> userListCall = apiService.getUserList(searchParams);
+        userListCall.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                if (response.isSuccessful()) {
+                    UserList userList = response.body();
+                    prepareData(userList);
+                } else {
+
+                    Toast.makeText(MainActivity.this,
+                            "Request not Sucessful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                Toast.makeText(MainActivity.this,
+                        "Request failed. Check your internet connection",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
     @Override
